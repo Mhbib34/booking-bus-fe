067d591d81938782e0d7565b0cf8bb32433fb038
Mhbib34 <mhabib34official@gmail.com>
2025-09-27 16:01:58 +0700

    feat : add user profile settings

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -4,7 +4,7 @@ export type User = {
   phone?: string;
   full_name?: string;
   password?: string;
-  created_at?: Date;
+  created_at?: string;
   updated_at?: Date;
   role?: string;
   confirmPassword?: string;
